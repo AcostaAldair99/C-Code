aeaa9dd404155b169547f5213216247adca0d452
e-aacosta <aldair.acostafm@gmail.com>
2023-07-26 23:26:45 -0600

    Feature: new implementatio of a queue based on a linked list

---
+++ Data-Structures/Queue/queueDinamic.c
@@ -0,0 +1,114 @@
+//This is a implementation of a dinamic
+//queue based in a linked list 
+
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include<string.h>
+#include <sys/types.h>
+
+
+typedef struct item{
+   int value;
+   struct item *pNext;
+} Item; 
+
+
+Item * createItem(int value);
+void enqueue(Item **head,Item **tail,int value);
+void dequeue(Item **head);
+void dequeueTail(Item **head,Item **tail);
+void display(Item **head,Item **tail);
+
+
+
+int main(){
+    Item *head,*tail = NULL;
+    
+    enqueue(&head,&tail,10);
+    enqueue(&head,&tail,12);   
+    enqueue(&head,&tail,13);
+    enqueue(&head,&tail,14);
+    enqueue(&head,&tail,15);
+ 
+    display(&head,&tail);
+
+    dequeue(&head);
+
+    display(&head,&tail);
+    enqueue(&head,&tail,22);
+    // dequeueTail(&head,&tail);
+    display(&head,&tail);
+    
+}
+
+void dequeue(Item **head){
+   Item *current; 
+   if(*head == NULL){
+        printf("The queue is empty !!\n");
+       return;
+   }
+   current = *head; 
+    *head = current->pNext;
+    free(current);
+    return;
+}
+
+void dequeueTail(Item **head,Item **tail){
+    Item *current,*prev;
+    if(*head == NULL){
+        printf("The queue is empty !!\n");
+        return;
+    }
+    current = *head;
+    while(current->pNext != NULL){
+        prev = current;
+        current = current->pNext;
+    }
+     printf("Prev: %d - Current: %d\n",prev->value,current->value);
+    *tail = prev;
+    free(current);
+    return;
+}
+
+void enqueue(Item **head,Item **tail,int value){
+    Item *current,*new;
+    new = createItem(value);
+    if(*head ==  NULL){
+        *head = new;
+        *tail = new;
+        return;
+    }
+
+    current = *head; 
+    
+    while(current->pNext !=  NULL){
+        current = current->pNext;
+    }
+    current->pNext = new;
+    *tail = new;
+    return;
+}
+
+
+Item * createItem(int value){
+   Item *item = (Item*)malloc(sizeof(Item));
+   item->pNext = NULL; 
+   item->value = value;
+   return item; 
+}
+
+
+void display(Item **head,Item **tail){
+    Item *current;
+    current = *head;
+    printf("\n->HEAD: %d -- TAIL: %d\n\n",(*head)->value,(*tail)->value);
+
+    while(current != NULL){
+        printf("%d - ",current->value);
+        current = current->pNext;
+    }
+    printf("\n");
+}
+
+
