7a748ef6b254c6859fe0fe7e8f19a3269c250e6d
e-aacosta <aldair.acostafm@gmail.com>
2023-08-17 18:09:01 -0600

    New implementation of a handler of collision in the hash table

--- Data-Structures/HashTable/main.c
+++ Data-Structures/HashTable/main.c
@@ -1,28 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 
-
-#define SIZE 5 
+#define SIZE 10 
 
 int hashMultMethod(int value);
 int hashModMethod(int value);
 int search(int *array,int value);
 int getIndex(int value);
 void addToArray(int *array ,int value);
+void addItem(int *array,int value,int hashIndex);
 int * createArray();
 void display(int *array);
 
 int main(){
     int *array = createArray();
     
-    for(int i = 0;i < SIZE;i++){
-        addToArray(array,i+SIZE);
-    }
-
+    addToArray(array,15);
+    addToArray(array,13);
+    addToArray(array,12);
+    addToArray(array,19);
+    addToArray(array,15);
+    addToArray(array,15);
+    addToArray(array,15);
+    addToArray(array,15);
+    addToArray(array,10);
+    addToArray(array,10);        
+    addToArray(array,10);
+    addToArray(array,10);
     display(array);
-    printf("Value Searched: %d\nIs in the Index: %d\n",search(array,14),getIndex(14));
-    printf("\n%d:",5%10);
 }
 
 
@@ -40,21 +47,27 @@ int getIndex(int value){
 
 //Using the mod method for hashing
 int search(int *array,int value){
-    return array[hashModMethod(value)]; 
+    return array[value % SIZE]; 
 }
 
 //Using the mod method for hashing
 void addToArray(int *array,int value){
-    array[hashModMethod(value)] = value;
+    //Here we try to handle collisions using Open Addressing 
+    addItem(array,value,value%SIZE); 
 }
 
-
-//Hashing with the mult method
-int hashModMethod(int value){
-    return value % SIZE; 
+void addItem(int *array,int value,int hashIndex){
+    if(array[hashIndex] == 0 ){
+        array[hashIndex] = value; 
+        printf("We add the value: %d\n",value);
+        return ;
+    }
+    if(hashIndex == (SIZE-1)){
+        printf("There is not slots available!!\n");
+        return;
+    }
+    addItem(array,value,hashIndex+1);
 }
-
-
 
 void display(int *array){
     printf("\n");
